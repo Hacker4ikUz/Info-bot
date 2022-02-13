18791347fe19cc9020c4c2c1c92ed6e97258ad8f
Salomov Suxrob <59762516+Hacker4ikUz@users.noreply.github.com>
2022-02-13 17:54:34 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('TOKEN', '5049648399:AAH3ffUNhX9iVuDmqlw2m1Aq48cZBRvJBTc');
+$command = '/info';
+$ip = '82.148.4.58';
+$port = '27017';
---
+++ mon.php
@@ -0,0 +1,19 @@
+<?php
+include 'config.php';
+include 'query/Buffer.php';
+include 'query/BaseSocket.php';
+include 'query/Socket.php';
+include 'query/SourceQuery.php';
+$sq = new SourceQuery();
+
+$sq->Connect($ip, $port, 1, SourceQuery::GOLDSOURCE);
+$aData = $sq->GetInfo();
+$aData['Players'] = $aData['Players'] < 1 ? 0 : $aData['Players'];
+$aData['status'] = strlen($aData['Map']) > 3 ? ''.$aData['Map'].'' : 'не определена';
+$output .= '
+	'.$aData['HostName'].'🔥
+	IP: '.$ip.':'.$port.'⚡️
+	Карта: '.$aData['status'].' 🗾
+	Игроки: '.$aData['Players'].' из '.$aData['MaxPlayers'].'😉
+	Гл.Админ: @Kadirkulov 😎
+';
